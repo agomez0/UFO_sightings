0706a6b0780270138402451ff36d6236548e7108
Angie Gomez <angie@Angies-MacBook-Air.local>
2019-04-25 10:25:07 -0700

    updated comments

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,115 +1,130 @@
-// // from data.js
-// var tableData = data;
+// from data.js
+var tableData = data;
 
-// // YOUR CODE HERE!
-// var tbody = d3.select("tbody");
-// var filter = d3.select("#filter-btn");
+// Select the table body tag
+var tbody = d3.select("tbody");
+// Select the filter button
+var filter = d3.select("#filter-btn");
 
-// tableData.forEach(ufo => {
-//     var row = tbody.append("tr");
-//     Object.entries(ufo).forEach(function([key, value]) {
-//         var td = row.append("td").text(value);
-//     });
-// });
+// Append info for each ufo sighting to the table body
+tableData.forEach(ufo => {
+    //Create new row for each ufo sighting
+    var row = tbody.append("tr");
+    Object.entries(ufo).forEach(function([key, value]) {
+        //Append all values of the ufo sighting to the row
+        var td = row.append("td").text(value);
+    });
+});
 
-// d3.selectAll(".filter").on("change", updateFilters);
-// filter.on("click", updateFilters);
+// Run function when there is a change in any of the filter inputs
+d3.selectAll(".filter").on("change", updateFilters);
+// Run function when the filter button is clicked
+filter.on("click", updateFilters);
 
-// var filters = {};
+//Create empty dictionary
+var filters = {};
 
-// function updateFilters() {
+function updateFilters() {
 
-//     d3.event.preventDefault();
+    d3.event.preventDefault();
 
-//     var changedElement = d3.select(this).select("input");
-//     var elementValue = changedElement.property("value");
-//     var filterId =  changedElement.attr("id");
+    // Selects the input object
+    var changedElement = d3.select(this).select("input");
+    // Selects the input text value
+    var elementValue = changedElement.property("value");
+    // Selects the input id name
+    var filterId =  changedElement.attr("id");
 
-//     if (elementValue) {
-// 	    filters[filterId] = elementValue;
-//     } 
-//     else {
-// 	    delete filters[filterId];
-//     }
+    // If the element value exists
+    //Add it to the dictionary with the id as the key
+    if (elementValue) {
+	    filters[filterId] = elementValue;
+    } 
+    // Else delete the entry in the dictionary
+    else {
+	    delete filters[filterId];
+    }
 
-//     // call the filterTable function
-//     filterTable();
-// }
+    // call the filterTable function
+    filterTable();
+}
 
-// function filterTable() {
-//     var filteredData = tableData;
-//     // code to filter table using 'filters'
-//     Object.entries(filters).forEach(([key, value]) => {
-//         filteredData = filteredData.filter(row => row[key] === value);
-//       });
-
-//     tbody.html("");
-
-//     filteredData.forEach(ufo => {
-//         var row2 = tbody.append("tr");
-//         Object.entries(ufo).forEach(function([key, value]) {
-//             var td2 = row2.append("td").text(value);
-//         });
-//       });
-// }
+function filterTable() {
+    var filteredData = tableData;
+    // code to filter table using 'filters'
+    Object.entries(filters).forEach(([key, value]) => {
+        filteredData = filteredData.filter(row => row[key] === value);
+      });
+    
+    // Clear current table
+    tbody.html("");
+    
+    // Add all the filtered data to the tabe body
+    filteredData.forEach(ufo => {
+        var row2 = tbody.append("tr");
+        Object.entries(ufo).forEach(function([key, value]) {
+            var td2 = row2.append("td").text(value);
+        });
+      });
+}
 
 
 //-------------------------------------------------------------------------------
 
-var tableData = data;
+// var tableData = data;
 
-var button = d3.select("#filter-btn");
-var tbody = d3.select("tbody");
+// var button = d3.select("#filter-btn");
+// var tbody = d3.select("tbody");
 
-tableData.forEach (item => {
-    var row = tbody.append("tr");
-    Object.entries(item).forEach(([key, value]) => {
-        var dt = row.append("td").text(value);
-    });
-});
+// tableData.forEach (item => {
+//     var row = tbody.append("tr");
+//     Object.entries(item).forEach(([key, value]) => {
+//         var dt = row.append("td").text(value);
+//     });
+// });
 
 
-button.on("click", updateFilters);
+// button.on("click", updateFilters);
 
-var filters = {};
+// var filters = {};
 
-function updateFilters() {
-    d3.event.preventDefault();
-    var all = d3.selectAll("li").select("input");
+// function updateFilters() {
+//     d3.event.preventDefault();
+//     var all = d3.selectAll("li").select("input");
 
-    var items = all._groups;
+//     var items = all._groups;
 
-    items.forEach(item => {
-        var input = item.property("value");
-        var itemId = item.property("id");
+//     items.forEach(item => {
+//         var input = item.property("value");
+//         var itemId = item.property("id");
 
-        if (input) {
-            filters[itemId] = input;
-        }
-        else {
-            delete filters[itemId];
-        }
+//         if (input) {
+//             filters[itemId] = input;
+//         }
+//         else {
+//             delete filters[itemId];
+//         }
 
-    });
+//     });
     
-    updateTable();
-}
+//     updateTable();
+// }
 
-function updateTable() {
+// function updateTable() {
 
-    tbody.html("");
+//     tbody.html("");
 
-    var filteredData = tableData;
+//     var filteredData = tableData;
 
-    Object.entries(filters).forEach(([key,value]) => {
-        filteredData = filteredData.filter(row => row[key] === value);
-    });
+//     Object.entries(filters).forEach(([key,value]) => {
+//         filteredData = filteredData.filter(row => row[key] === value);
+//     });
 
-    filteredData.forEach(ufo => {
-        var row2 = tbody.append("tr");
-        Object.entries(ufo).forEach(([key, value]) => {
-            var td2 = row2.append("td").text(value);
-        });
-    });
-}
+//     filteredData.forEach(ufo => {
+//         var row2 = tbody.append("tr");
+//         Object.entries(ufo).forEach(([key, value]) => {
+//             var td2 = row2.append("td").text(value);
+//         });
+//     });
+// }
 
